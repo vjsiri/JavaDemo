8e2532a73f3366ca8d3ac8f87fd375f095b25c19
vijay <vjsiri17@gmail.com>
2021-05-23 03:28:24 -0400

    initial proj template

---
+++ src/main/java/com/ibm/app/JavaApp/App.java
@@ -0,0 +1,13 @@
+package com.ibm.app.JavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
